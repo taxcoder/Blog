82847e63ce9af990d8e8ed4526d318ecd7541373
taxcoder <1571922819@qq.com>
2024-12-07 07:48:18 +0000

    init

--- blog-web/apis/src/env/prod.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  BASE_URL: 'https://www.recall.top',
-  TIME_OUT: 1000 * 10,
-};
--- blog-web/utils/index.ts
+++
@@ -1,8 +0,0 @@
-//@ts-ignore
-import useCommon from './src/common';
-//@ts-ignore
-import useDate from './src/day';
-//@ts-ignore
-import usePage from './src/page';
-
-export { useCommon, useDate, usePage };
